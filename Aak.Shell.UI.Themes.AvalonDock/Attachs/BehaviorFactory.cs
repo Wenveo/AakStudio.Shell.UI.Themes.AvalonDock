6048a44fabda5fa6f28bf09beb625abf6ec02de8
Noisrev <Noisrev@outlook.com>
2023-01-14 20:01:50 +0800

    Fix the `handleERASEBKGND` in `WindowChromeBehavior`

--- Aak.Shell.UI.Themes.AvalonDock/Attachs/BehaviorFactory.cs
+++ Aak.Shell.UI.Themes.AvalonDock/Attachs/BehaviorFactory.cs
@@ -1,13 +1,34 @@
-﻿using ControlzEx.Behaviors;
+﻿using System.Reflection;
+
+using ControlzEx.Behaviors;
 
 namespace Aak.Shell.UI.Themes.AvalonDock.Attachs
 {
     internal static class BehaviorFactory
     {
+        // See: https://github.com/ControlzEx/ControlzEx/blob/develop/src/ControlzEx/Behaviors/WindowChrome/WindowChromeBehavior.MessageHandling.cs#L447
+        private sealed class FixWindowChromeBehavior : WindowChromeBehavior
+        {
+            protected override void OnAttached()
+            {
+                base.OnAttached();
+
+                AssociatedObject.Loaded += AssociatedObject_Loaded;
+            }
+
+            private void AssociatedObject_Loaded(object sender, System.Windows.RoutedEventArgs e)
+            {
+                AssociatedObject.Loaded -= AssociatedObject_Loaded;
+
+                var handleERASEBKGND = typeof(WindowChromeBehavior).GetField("handleERASEBKGND", BindingFlags.NonPublic | BindingFlags.Instance);
+                handleERASEBKGND.SetValue(this, false);
+            }
+        }
+
         public static GlowWindowBehavior CreateGlowWindowBehavior()
         => new() { IsGlowTransitionEnabled = true };
 
         public static WindowChromeBehavior CreateWindowChromeBehavior()
-        => new();
+        => new FixWindowChromeBehavior();
     }
 }
